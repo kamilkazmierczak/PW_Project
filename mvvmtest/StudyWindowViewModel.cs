6b8ca6a454bdd99ac205d6f4b7df95718d7a4651
Kamil Kaźmierczak <Kamil Kamierczak>
2016-10-12 01:29:47 +0200

    StudyWindowViewModel with control on others(UserList)

--- mvvmtest/StudyWindowViewModel.cs
+++ mvvmtest/StudyWindowViewModel.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kazmierczak.Languer.Interfaces;
+using System.ComponentModel;
+using System.Windows;
 
 namespace Kazmierczak.Languer.UI
 {
     public class StudyWindowViewModel : ObservableObject, IPageViewModel
     {
+
+        //private WordInsertViewModel _wordInsertViewModel;
+        private UserListViewModel _userListViewModel;
+        //private IDAO _dao;
+
+        public StudyWindowViewModel()
+        {
+            #if DEBUG
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject())) return;
+            #endif
+
+            _userListViewModel = new UserListViewModel();
+        }
+
+        public UserListViewModel UserListViewModel
+        {
+            get { return _userListViewModel; }
+            set
+            {
+                _userListViewModel = value;
+                RaisePropertyChanged("UserListViewModel");
+            }
+        }
+
+
         public string Name
         {
             get
