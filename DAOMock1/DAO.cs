5910e3aff708594e3ebffdf165cfa42443272d18
Kamil Kaźmierczak <Kamil Kamierczak>
2016-10-11 18:15:49 +0200

    before major changes in DB to fix List to work with entities

--- DAOMock1/DAO.cs
+++ DAOMock1/DAO.cs
@@ -64,43 +64,37 @@ namespace Kazmierczak.Languer.DAO
         {
             var currentUser = CurrentOptions.CurrentUser;
 
-            using (var context = new DataContext())
-            {
-                //var query = context.Users.Select(x => x.UserID == currentUser.UserID);
-                //var user = (IUser)query.First<User>();
-                var query = context.Users.Where(x => x.UserID == currentUser.UserID);
-                User user = new User();
-                try{
-                    user = query.FirstOrDefault<User>();
-                }catch (Exception){
-                    Console.WriteLine("User not found");
-                    return;
-                }
-
-                var dictionariesIDs = context.Dictionaries.Select(x => x.DictionaryID);
-                dictionary.DictionaryID = dictionariesIDs == null ? dictionariesIDs.Max() + 1 : 1;
-
-                //Console.WriteLine(dictionary.Name);
-                
-                user.DictionariesList.Add(dictionary);
-              
-
-                //Console.WriteLine("wybrales");
-                //Console.WriteLine(user.Name);
-
-                //Console.WriteLine(user.);
-                //user.UserID = usersIDs == null ? usersIDs.Max() + 1 : 1;
-                //context.Users.Add(user as User);
-                //context.SaveChanges();
-            }
-
             //using (var context = new DataContext())
             //{
-            //    var dictionariesIDs = context.Dictionaries.Select(x => x.DictionaryID);
-            //    dictionary.DictionaryID = dictionariesIDs == null ? dictionariesIDs.Max() + 1 : 1;
-            //    context.Dictionaries.Add(dictionary as Dictionary);
-            //    context.SaveChanges();
+            //    var query = context.Users.SingleOrDefault(x => x.UserID == currentUser.UserID);
+            //    if (query != null)
+            //    {
+            //        //User user = new User();
+            //        var dictionariesIDs = context.Dictionaries.Select(x => x.DictionaryID);
+            //        dictionary.DictionaryID = dictionariesIDs == null ? dictionariesIDs.Max() + 1 : 1;
+            //        //user.DictionariesList.Add(dictionary);
+            //        query.DictionariesList.Add(dictionary);
+            //        int length = query.DictionariesList.Count();
+            //        context.SaveChanges();
+            //        Console.WriteLine("Dodalem do usera" + query.Name + "slownik" + query.DictionariesList[length - 1].Name);
+            //    }
             //}
+
+
+            using (var context = new DataContext())
+            {
+                var query = context.Users.SingleOrDefault(x => x.UserID == currentUser.UserID);
+                if (query != null)
+                {
+                    foreach (var item in query.DictionariesList)
+                    {
+                        Console.WriteLine("DictionaryID: "+item.DictionaryID+" Name: "+item.Name);
+                    }
+                }
+            }
+
+
+
         }
     }
 }
