144b95d076587ecb8bf8925125b7be7eaeb8e030
Kamil Kaźmierczak <Kamil Kamierczak>
2016-10-12 20:09:54 +0200

    statistics bind with strings variables

--- mvvmtest/StatisticsShowViewModel.cs
+++ mvvmtest/StatisticsShowViewModel.cs
@@ -11,6 +11,8 @@ namespace Kazmierczak.Languer.UI
     public class StatisticsShowViewModel : ObservableObject
     {
         private DictionaryViewModel _selectedDictionary;
+        private string _correctPercentage;
+        private string _incorrectPercentage;
 
         public StatisticsShowViewModel()
         {
@@ -18,7 +20,29 @@ namespace Kazmierczak.Languer.UI
             if (DesignerProperties.GetIsInDesignMode(new DependencyObject())) return;
             #endif
 
+            _correctPercentage = "A";
+            _incorrectPercentage = "B";
 
+        }
+
+        public string CorrectPercentage
+        {
+            get { return _correctPercentage; }
+            set
+            {
+                _correctPercentage = value;
+                RaisePropertyChanged("CorrectPercentage");
+            }
+        }
+
+        public string IncorrectPercentage
+        {
+            get { return _incorrectPercentage; }
+            set
+            {
+                _incorrectPercentage = value;
+                RaisePropertyChanged("IncorrectPercentage");
+            }
         }
 
         public DictionaryViewModel SelectedDictionary
