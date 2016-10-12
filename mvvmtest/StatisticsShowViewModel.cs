ca7648733f5b87770cb4b801824cecf033851af3
Kamil Kaźmierczak <Kamil Kamierczak>
2016-10-12 20:01:33 +0200

    changes in Statistics

---
+++ mvvmtest/StatisticsShowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kazmierczak.Languer.UI
+{
+    public class StatisticsShowViewModel : ObservableObject
+    {
+        private DictionaryViewModel _selectedDictionary;
+
+        public StatisticsShowViewModel()
+        {
+            #if DEBUG
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject())) return;
+            #endif
+
+
+        }
+
+        public DictionaryViewModel SelectedDictionary
+        {
+            get { return _selectedDictionary; }
+            set
+            {
+                _selectedDictionary = value;
+                Console.WriteLine("HOHOHO"+_selectedDictionary.Name);
+                RaisePropertyChanged("SelectedDictionary");
+            }
+        }
+    }
+}
--- mvvmtest/StatisticsWindowViewModel.cs
+++ mvvmtest/StatisticsWindowViewModel.cs
@@ -4,12 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kazmierczak.Languer.Interfaces;
+using System.ComponentModel;
+using System.Windows;
 
 namespace Kazmierczak.Languer.UI
 {
     public class StatisticsWindowViewModel : ObservableObject, IPageViewModel
     {
 
+        private DictionaryListViewModel _dictionaryListViewModel;
+        private StatisticsShowViewModel _statisticsShowViewModel;
+
+        public StatisticsWindowViewModel()
+        {
+            #if DEBUG
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject())) return;
+            #endif
+
+            _statisticsShowViewModel = new StatisticsShowViewModel();
+            _dictionaryListViewModel = new DictionaryListViewModel();
+        }
+
+
+        public StatisticsShowViewModel StatisticsShowViewModel
+        {
+            get { return _statisticsShowViewModel; }
+            set
+            {
+                _statisticsShowViewModel = value;
+                RaisePropertyChanged("StatisticsShowViewModel");
+            }
+        }
+
+        public DictionaryListViewModel DictionaryListViewModel
+        {
+            get { return _dictionaryListViewModel; }
+            set
+            {
+                _dictionaryListViewModel = value;
+                RaisePropertyChanged("DictionaryListViewModel");
+            }
+        }
+
+      
         public string Name
         {
             get
